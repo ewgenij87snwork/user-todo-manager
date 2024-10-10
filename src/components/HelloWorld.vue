db454ae92f86de7c41eb2ec6dfaca4d5bf74be88
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-10-10 17:57:06 +0300

    ADD: Init Vue, Webpack, Loaders, TS

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+import { Ref, ref } from "vue";
+
+interface Some {
+  str: string,
+  num: number
+}
+
+const btnClick = (a: number) => {
+  someNum.value.num = a;
+}
+
+let someNum: Ref<Some> = ref({
+  str: '',
+  num: 0
+});
+const someStr: string = 'dddd';
+
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <button @click="btnClick(someStr)">btn</button>
+  <h2>{{ someNum.num }}</h2>
+</template>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.ts
@@ -0,0 +1,73 @@
+import path from 'path';
+import webpack from 'webpack';
+import { VueLoaderPlugin } from 'vue-loader';
+import 'webpack-dev-server';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+interface EnvVariables {
+    mode?: 'production' | 'development',
+    port?: number
+}
+
+
+
+export default (env: EnvVariables) => {
+    let config: webpack.Configuration;
+    const isDev = env.mode === 'development';
+
+    config = {
+        entry: path.resolve(__dirname, 'src', 'main.ts'),
+        mode: env.mode ?? 'development',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+        resolve: {
+            extensions: [
+                '.ts',
+                '.js',
+                '.vue'
+            ],
+            alias: {
+                '@': path.resolve(__dirname, "./src/"),
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                },
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                    },
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html'),favicon: path.resolve(__dirname, 'public', 'favicon.ico') }),
+        ],
+        devtool: isDev && 'inline-source-map',
+        devServer: {
+            historyApiFallback: true,
+            static: {
+                directory: path.resolve(__dirname, 'dist'),
+            },
+            hot: true,
+        },
+    };
+
+    return config;
+}
